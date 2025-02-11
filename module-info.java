4fc427fb5cc2f6f500c05972def24d07313589ce
Leevi Niinimäki <leewi99@gmail.com>
2025-02-12 00:11:29 +0200

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module KIVIPAPERISAKSET {
-	requires javafx.controls;
-	requires javafx.fxml;
-	 
-	exports application;
-	exports application.controller;
-	
-	opens application.controller to javafx.fxml;
-}
